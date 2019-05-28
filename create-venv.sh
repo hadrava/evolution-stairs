9082353b4788aad26c0f79d61e2c49a4ba9a693b
Jan Hadrava <had@kam.mff.cuni.cz>
2019-05-28 15:37:57 +0200

    venv: specify versions for gym + roboschool

--- create-venv.sh
+++ create-venv.sh
@@ -1,7 +1,7 @@
 python3 -m venv VENV_DIR
 . startvenv.sh
-pip install gym
-pip install roboschool
+pip install gym==0.12.1
+pip install roboschool==1.0.46
 VENV_DIR/bin/pip3 install tensorflow==2.0.0a0
 #pip install tensorflow==1.2.0 #--ignore-installed
 
